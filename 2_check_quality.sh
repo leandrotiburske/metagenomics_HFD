43e373af34fb6be416abf5bf27aa023fb96c3c16
leandrotiburske <leandrotiburske@usp.br>
2023-09-20 15:39:23 +0200

    test

--- 2_check_quality.sh
+++ 2_check_quality.sh
@@ -13,13 +13,14 @@ module load bioinfo-tools
 module load FastQC/0.11.5
 module load MultiQC/1.7
 
-# Each sample (R1 and R2 files) must be within an individual directory indise "input"
-# Run code in the Analysis directory  
-# run fastqc and multiqc
+# Each sample (R1 and R2 files) must be within an individual directory inside "input"
+# Note: Run code in the Analysis directory  
+# Run fastqc and multiqc
 mkdir -p fastqc_out
 
 cd input/
 
+# Performs FastQC separately for R1 and R2 files
 for fastq in ./* 
 do
     fastqc -o ../fastqc_out $fastq/$(basename "$fastq")_1.fastq.gz  
@@ -28,5 +29,6 @@ done
 
 cd ../fastqc_out/
 
+# Run MultiQC for R1 and R2 FastQC outputs separately
 multiqc *_1_fastqc.zip -o forward_multiqc
 multiqc *_2_fastqc.zip -o reverse_multiqc
